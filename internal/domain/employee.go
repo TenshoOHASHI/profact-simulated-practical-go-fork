0e18d368ccf3f76b805f952effb210c5805c80ad
tensho <75622122+tensho9408@users.noreply.github.com>
2026-03-01 17:15:25 +0900

    feat(employee): update domain entity and add repository #6

--- internal/domain/employee.go
+++ internal/domain/employee.go
@@ -3,19 +3,19 @@ package domain
 import "time"
 
 type Employee struct {
-	ID            string    `json:"id"`
-	Name          string    `json:"name"`
-	Email         string    `json:"email"`
-	Password_hash string    `json:"-"`
-	Role          string    `json:"role"`
-	CreatedAt     time.Time `json:"created_at"`
-	UpdatedAt     time.Time `json:"updated_at"`
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	Role         string    `json:"role"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
 
-type EmployeesRepository interface {
+type EmployeeRepository interface {
 	FindAll(limit, offset int) ([]*Employee, error)
 	FindByID(id string) (*Employee, error)
-	Create(employees *Employee) error
-	Update(employees *Employee) error
+	Create(employee *Employee) error
+	Update(employee *Employee) error
 	Delete(id string) error
 }
